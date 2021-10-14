ffd9645867a8c581dac937fae18509216276f94c
Repan Biswas <contactrepan@gmail.com>
2021-10-15 02:11:46 +0600

    initial commit

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,11 +1,11 @@
 console.log(process.env);
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
-};
+    apiKey: "AIzaSyA0KPvm1eV-Fcfyj1cQVNYVNSo6gxP6o4Q",
+    authDomain: "ema-john-react-auth-9be78.firebaseapp.com",
+    projectId: "ema-john-react-auth-9be78",
+    storageBucket: "ema-john-react-auth-9be78.appspot.com",
+    messagingSenderId: "652131597002",
+    appId: "1:652131597002:web:7bae68d48337a4dc481233"
+  };
 
 export default firebaseConfig;
